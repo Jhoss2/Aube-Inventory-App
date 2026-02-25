0aecb481c653e776b53caee2ed09ee97f38d9beb
Jhoss2 <Jhoss8631@gmail.com>
2026-02-25 00:51:40 +0000

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-import { registerRootComponent } from 'expo';
-import App from './App';
-
-// Cela garantit que l'application est correctement enregistrée, 
-// peu importe si vous êtes sur Expo Go ou en APK natif.
-registerRootComponent(App);
