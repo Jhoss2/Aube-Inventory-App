81451f9676251939f312f5dce2f3ff65c50b9f26
Jhoss2 <Jhoss8631@gmail.com>
2026-02-25 01:14:36 +0000

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,5 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/utils/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// On ajoute la configuration NativeWind par-dessus la config par défaut
-module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = config;
