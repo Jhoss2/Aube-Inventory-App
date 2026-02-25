d9d5ace19598acada0433bf90e7936fd97c51721
Jhoss2 <Jhoss8631@gmail.com>
2026-02-25 01:01:00 +0000

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -5,43 +5,25 @@ const config: ExpoConfig = {
   slug: "uaubentracker",
   version: "1.0.0",
   orientation: "portrait",
-  icon: "./assets/images/icon.png",
   userInterfaceStyle: "light",
-  splash: {
-    image: "./assets/images/splash-icon.png",
-    resizeMode: "contain",
-    backgroundColor: "#ffffff"
-  },
   assetBundlePatterns: ["**/*"],
   ios: {
     supportsTablet: true,
     bundleIdentifier: "com.uauben.tracker"
   },
   android: {
-    adaptiveIcon: {
-      foregroundImage: "./assets/images/icon.png",
-      backgroundColor: "#ffffff"
-    },
     package: "com.uauben.tracker",
     versionCode: 1,
-    permissions: ["android.permission.INTERNET", "android.permission.READ_EXTERNAL_STORAGE", "android.permission.WRITE_EXTERNAL_STORAGE"]
-  },
-  web: {
-    favicon: "./assets/images/favicon.png"
+    permissions: [
+      "android.permission.INTERNET",
+      "android.permission.READ_EXTERNAL_STORAGE",
+      "android.permission.WRITE_EXTERNAL_STORAGE"
+    ]
   },
   plugins: [
     "expo-router",
     "expo-sqlite",
-    "expo-font",
-    [
-      "expo-splash-screen",
-      {
-        "image": "./assets/images/splash-icon.png",
-        "imageWidth": 200,
-        "resizeMode": "contain",
-        "backgroundColor": "#ffffff"
-      }
-    ]
+    "expo-font"
   ],
   experiments: {
     typedRoutes: true
