2f33a0640b2566cb936074116718c12baa96f688
Jhoss2 <Jhoss8631@gmail.com>
2026-02-27 03:13:21 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-import "expo-router/entry";
+import { registerRootComponent } from 'expo';
+import { ExpoRoot } from 'expo-router';
+
+// Force le chargement du dossier /app (Expo Router)
+export function App() {
+  const ctx = require.context('./app');
+  return <ExpoRoot context={ctx} />;
+}
+
+registerRootComponent(App);
